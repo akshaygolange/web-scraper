9e012111a4b5242f429d6dc4ecf4c7cd101d75ce
akshay <akshaygolange383@email.com>
2026-08-20 13:27:52 +0530

    Run Playwright in headless mode

--- backend/scrapers/browser.js
+++ backend/scrapers/browser.js
@@ -3,6 +3,11 @@ import { chromium } from "playwright";
 export const createBrowser = async () => {
   const browser = await chromium.launch({
     headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+    ],
   });
 
   return browser;
