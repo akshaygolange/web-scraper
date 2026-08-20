91350b5929003716787da12bf5c79cdc38e78f25
akshay <akshaygolange383@email.com>
2026-08-20 13:12:19 +0530

    Run Playwright in headless mode

--- backend/scrapers/browser.js
+++ backend/scrapers/browser.js
@@ -2,7 +2,7 @@ import { chromium } from "playwright";
 
 export const createBrowser = async () => {
   const browser = await chromium.launch({
-    headless: false,
+    headless: true,
   });
 
   return browser;
